575252caa9b140b0f271d1f432008fe95e4b6173
t-leyangxue <t-leyangxue@microsoft.com>
2022-06-24 14:57:29 +0100

    memcpy before write

--- lib/include/falcon/common/AsyncFileSink.h
+++ lib/include/falcon/common/AsyncFileSink.h
@@ -10,6 +10,26 @@
 #include <future>
 #include <vector>
 #include <functional>
+#include <mutex>
+
+struct aiocb asyncRequest(int fd,  
+                            off_t offset, 
+                            volatile void * content, 
+                            size_t length){
+    // create and initialize the aiocb structure.
+    // If we don't init to 0, we have undefined behavior.
+    // E.g. through sigevent op.aio_sigevent there could be 
+    //      a callback function being set, that the program
+    //      tries to call - which will then fail.
+    struct aiocb ret = {0};
+    {
+        ret.aio_fildes = fd;
+        ret.aio_offset = offset;
+        ret.aio_buf = content;
+        ret.aio_nbytes = length;            
+    }
+    return ret;
+}
 
 template <typename SampleType>
 class AsyncFileSink : public FileSink<SampleType> {
@@ -18,18 +38,26 @@ public:
   AsyncFileSink(const AsyncFileSink&) = delete; //prevent copy
   AsyncFileSink& operator=(const AsyncFileSink&) = delete; //prevent copy
   virtual ~AsyncFileSink() {
-    for (auto& fut : aiolist) {
-      fut.get();
+    for (auto& obj : aiolist) {
+      obj.get();
+      // delete[] obj.second;
     }
     aiolist.clear();
   }
 
   virtual size_t write(SampleType* buffer, size_t nSamples) override {
-    std::future<long unsigned int> fut = std::async(std::launch::async, &FileSink<SampleType>::write, this, buffer, nSamples);
+    SampleType* async_buffer = new SampleType[nSamples];
+    memcpy(async_buffer, buffer, sizeof(SampleType) * nSamples);
+    auto run_io = [this, &async_buffer, &nSamples]() {
+      FileSink<SampleType>::write(async_buffer, nSamples);
+      delete[] async_buffer;
+    };
+    std::future<void> fut = std::async(std::launch::async, run_io);
     aiolist.push_back(std::move(fut));
     return nSamples; // async call nothing to return now
   }
 
 private:
-  std::vector<std::future<long unsigned int>> aiolist;
+  std::vector<std::future<void>> aiolist;
+  // std::vector<SampleType*> bufferlist;
 };
